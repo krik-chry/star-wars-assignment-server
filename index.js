3c0dcccc992bee2d36dde6111dc032240ba18264
krik-chry <krikonis.chrisostomos@gmail.com>
2020-01-12 16:13:39 +0100

    Search by film ID and sort characters by height, comments

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const router = require('./routes');
 
 const app = express();
 const port = 4000;
+const corsMiddleware = cors();
 
+app.use(corsMiddleware);
 app.use(router);
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
